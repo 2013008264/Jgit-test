0fc6502727bb93bf10b143f34c0351433d6ac29c
yd-lee <game9777@naver.com>
2018-07-28 12:44:49 +0900

    Test-fin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) throws GitAPIException {
         Git git;
         try {
-            git = Git.open(new File("D:/project/github-to-yobi-migration-service/sshtest/gitPractice"));
+            git = Git.open(new File("D:/project/github-to-yobi-migration-service/sshtest/2018_ELE3021_2013008264"));
         } catch (IOException e) {
             System.out.println("IOException occured");
             git = null;
@@ -36,7 +36,8 @@ public class Main {
             }
         };
         git.branchCreate()
-                .setName("TemporalBranchesss")
+                .setStartPoint("453e8715b40641a1ae0e4892a645afb617f5f2a2")
+                .setName("TemporalBranch")
                 .call();
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf1 = new SimpleDateFormat("hh:mm:ss");
@@ -45,18 +46,42 @@ public class Main {
                 .setPushAll()
                 .setTransportConfigCallback(transportConfigCallback)
                 .call();
-        Calendar cal1 = Calendar.getInstance();
-        System.out.println(sdf1.format(cal1.getTime()));
+        cal = Calendar.getInstance();
+        System.out.println(sdf1.format(cal.getTime()));
         git.branchDelete()
-                .setBranchNames("TemporalBranchesss")
+                .setBranchNames("TemporalBranch")
                 .call();
 
         git.push()
                 .setTransportConfigCallback(transportConfigCallback)
-                .setRefSpecs(new RefSpec(":TemporalBranchesss"))
+                .setRefSpecs(new RefSpec(":refs/heads/TemporalBranch"))
                 .call();
-        Calendar cal2 = Calendar.getInstance();
-        System.out.println(sdf1.format(cal2.getTime()));
+        cal = Calendar.getInstance();
+        System.out.println(sdf1.format(cal.getTime()));
+
+        git.branchCreate()
+                .setStartPoint("3d7015597f875ce0e220bfb2685912823c95bb3f")
+                .setName("TemporalBranch")
+                .call();
+
+        cal = Calendar.getInstance();
+        System.out.println(sdf1.format(cal.getTime()));
+        git.push()
+                .setPushAll()
+                .setTransportConfigCallback(transportConfigCallback)
+                .call();
+        cal = Calendar.getInstance();
+        System.out.println(sdf1.format(cal.getTime()));
+        git.branchDelete()
+                .setBranchNames("TemporalBranch")
+                .call();
+
+        git.push()
+                .setTransportConfigCallback(transportConfigCallback)
+                .setRefSpecs(new RefSpec(":refs/heads/TemporalBranch"))
+                .call();
+        cal = Calendar.getInstance();
+        System.out.println(sdf1.format(cal.getTime()));
 
     }
 }
